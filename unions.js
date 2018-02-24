d7c9d6e60330bad3b22db0ba290b2f98141359b2
Andrew L <alavaia@yahoo.com>
2018-02-23 19:11:36 -0500

    Fixed and polished animation on quickFind. Added colors and more animations. Now removes dead sets.

--- unions.js
+++ unions.js
@@ -1,5 +1,6 @@
 var unionAnimationQueue = []; // very slow -> n^2 Memory requirement
-var unionIntervalTimer; 
+var unionIntervalTimer = 1000; 
+var callNum = 0;
 
 $('input[name="union-begin"]').on('click', function() {
 	callUnion(
@@ -10,11 +11,13 @@ $('input[name="union-begin"]').on('click', function() {
 });
 
 function callUnion(arrSize, numUnions, pauseTime, unionAlgo) {
-	// empty existing DOM elements
-	$('.union-layer').html('');
 
-	// clear previous timer if set
+	// clear previous values if set
+	callNum = 0;
+	$('.union-layer').html('');
+	$('#union-header').html('#0 : union( 0 , 0 )');
 	clearInterval(unionIntervalTimer);
+	unionAnimationQueue.length = 0;
 
 	// Create an array and populate values with indices
 	var array = [];
@@ -29,12 +32,12 @@ function callUnion(arrSize, numUnions, pauseTime, unionAlgo) {
 	for (var i = 0; i < numUnions; i++) {
 		var p = Math.floor(Math.random() * arrSize);  // 0 through maxIntSize - 1
 	  var q = Math.floor(Math.random() * arrSize);  // 0 through maxIntSize - 1
-	  createUnionQF(array, p, q); 
 	  array.push(p);
 	  array.push(q);
 	  unionAnimationQueue.push(array.slice());
 	  array.pop();
 	  array.pop();
+	 	createUnionQF(array, p, q); 
 	}
 
 	unionIntervalTimer = drawUnions(pauseTime, unionAlgo);
@@ -46,30 +49,126 @@ function drawUnions(pauseTime, sortAlgo) {
 		if (unionAnimationQueue.length > 0) {
 			var q = unionAnimationQueue[0].pop();
 			var p = unionAnimationQueue[0].pop();
-			var array = unionAnimationQueue[0];
+			var array = unionAnimationQueue[0].slice(); // copy
 			unionAnimationQueue.shift();
 
-			console.log('union(' + p + ', ' + q + ') - ' + array);
-			
-			if (sortAlgo == "quickFind") {
+			callNum++;
+			$('#union-header').html('#' + callNum + ' : union( ' + p + ' , ' + q + ' )');
 
-				// move each element that is connected to array[p] to where q is
-				for(var i = 0; i < array.length; i++) {
-					if(isConnectedQF(array, p, i)) {
-						//var parentSet = $('#union-id-' + i).parent();
-						//var width = parentSet.width();
-						moveAnimate('#union-' + i, '#union-set-' + q, pauseTime);
-						//parentSet.width(width);	
-					}
+			if (sortAlgo == "quickFind") {
+				//console.log('union( ' + p + ' , ' + q + ' ) - ' +  array);
+
+				colorSets(array[p], array[q], 'blue');	
+
+				if(array[p] != array[q]) {
+					// move each element that is connected to array[p] to where q is
+					for(var i = 0; i < array.length; i++) {
+						if(isConnectedQF(array, p, i)) {
+							moveAnimate('#union-' + i, '#union-set-' + array[q], pauseTime);
+
+							// Double check that none of the sets are empty while traversing
+							// the array. Empty sets might be left if the animation is too fast.
+							/*if($('#union-set-' + i).children().length == 0) {
+								$('#union-set-' + i).remove(); 
+							}
+							*/
+						}
+					} 
+				} else {
+					setTimeout( function() {
+						colorSets(array[p], array[q], 'white');
+					}, pauseTime/2);
 				}
-				//$('#union-id-' + p).parent().hide();
 			}
 		}
 
 		if (unionAnimationQueue.length == 0) {
 			clearInterval(unionIntervalTimer);
 		}
+
 	}, pauseTime);
+}
+
+
+function redrawElements(array) {
+	for (var i = 0; i < array.length; i++) {
+		$('#union-' + i).appendTo($('#union-set-' + array[i]));
+	}
+}
+
+function removeAllEmptySets() {
+	$('.union-layer').children().each(function() { 	// for each set
+		if($(this).children().length == 0) { 					// if no elements
+			$(this).remove();														// remove set
+		}
+	});
+}
+
+function colorSets(p, q, color) {
+	setP = $('#union-set-' + p);
+	setQ = $('#union-set-' + q);
+	setP.css({
+		'background-color': color,
+	});
+	setQ.css({
+		'background-color': color,
+	});
+}
+
+
+// animates the moving of an element to a different parent/container div
+// to use: "moveAnimate('#ElementToMove', '#newContainer', 1000);
+function moveAnimate(element, newParent, animationTime){
+  // Allow passing in either a jQuery object or selector
+  element = $(element);
+  oldParent = element.parent();
+  newParent = $(newParent);
+
+  // get current position
+  var oldOffset = element.offset();
+
+  // clone element and place in final position (maintains width of parent)
+  var newElement = element.clone().appendTo(newParent); 
+
+  // get final position
+  var newOffset = newElement.offset();
+
+  // create another clone for animation purposes
+  var temp = element.clone().appendTo('body');
+  temp.css({
+    'position': 'absolute',
+    'background-color': 'red',
+    'left': oldOffset.left - 5,
+    'top': oldOffset.top - 5,
+    'z-index': 1000
+  });
+
+  // hide final position element until animation is complete
+  newElement.hide();
+
+  temp.animate({'top': newOffset.top - 5, 'left': newOffset.left - 30}, animationTime/2, function(){
+   	newElement.show(); 			// show element in final position
+   	element.remove(); 			// delete original element
+   	temp.remove(); 					// delete animation element
+   	if(oldParent.children().length == 0) { 
+   		elementSort(newParent);	// sort elements in new array only once
+   		removeAllEmptySets();
+   		newParent.css({					// reset background color
+   			'background-color': 'white',
+   		});
+   	}
+  });
+}
+
+function elementSort(parent) {
+	var array = [];
+	$(parent).children().each(function() {
+		array.push(parseInt($(this).text(), 10));
+	});
+	arrayMergeSort(array);
+	for(var i = 0; i < array.length; i++) {
+		$('#union-' + array[i]).appendTo($(parent));
+	}
 }
 
 // ----------------
@@ -87,7 +186,6 @@ function createUnionQF(array, p, q) {
 		for (var i = 0; i < array.length; i++) {
 			if (array[i] == pval) {
 				array[i] = qval;
-				//$('#union-' + i).detach().appendTo('.union-layer2');
 			}
 		}
 	}
--- util.js
+++ util.js
@@ -12,40 +12,51 @@ function newFilledArray(length, val) {
 	return array;
 }
 
-// animates the moving of an element to a different parent/container div
-// to use: "moveAnimate('#ElementToMove', '#newContainer', 1000);
-function moveAnimate(element, newParent, animationTime){
-    // Allow passing in either a jQuery object or selector
-    element = $(element);
-    newParent = $(newParent);
+function arrayMergeSort(array) {
+	aux = new Array(array.length);
+	mgSort(array, 0, array.length - 1);
+}
 
-    // get current position
-    var oldOffset = element.offset();
+function mgSort(array, lo, hi) { // recursive 
+	if (hi <= lo) {
+		return;
+	} else {
+		var mid = Math.floor(lo + (hi - lo)/2);
+		mgSort(array, lo, mid);
+		mgSort(array, mid + 1, hi);
+		rMerge(array, lo, mid, hi);
+	}
+}
 
-    // clone element and place in final position (maintains width of parent)
-    var newElement = element.clone().appendTo(newParent); 
+function rMerge(array, lo, mid, hi) {
+	var i = lo;
+	var j = mid + 1;
 
-    // get final position
-    var newOffset = newElement.offset();
+	// copy array to aux
+	for (var k = lo; k <= hi; k++) { 
+		aux[k] = array[k];
+	}
 
-    // create another clone for animation purposes
-    var temp = element.clone().appendTo('body');
-    temp.css({
-        'position': 'absolute',
-        'background-color': 'red',
-        'left': oldOffset.left - 5,
-        'top': oldOffset.top - 5,
-        'z-index': 1000
-    });
-
-    // hide final position element until animation is complete
-    newElement.hide();
-
-    temp.animate({'top': newOffset.top - 5, 'left': newOffset.left - 5}, animationTime/2, function(){
-       	newElement.show(); 	// show element in final position
-       	element.remove(); 	// delete original element
-       	temp.remove(); 		// delete animation element
-    });
-
-
+	for (var k = lo; k <= hi; k++) {
+		// entire first half exhausted
+		if (i > mid) {
+			array[k] = aux[j];
+			j++;
+		} 
+		// entire second half exhausted
+		else if (j > hi) {
+			array[k] = aux[i];
+			i++;
+		}
+		// value from second half is lower
+		else if (aux[j] < aux[i]) {
+			array[k] = aux[j];
+			j++;
+		}
+		// value from first half is equal or lower
+		else {
+			array[k] = aux[i];	
+			i++;
+		}
+	}
 }
